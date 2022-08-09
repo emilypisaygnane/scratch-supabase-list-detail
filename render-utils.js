1e512a07a385f1469ab6b8eeeebb241127fbf3b1
Emily <episaygnane@me.com>
2022-08-09 10:35:20 -0700

    added info for render utils

---
+++ render-utils.js
@@ -0,0 +1,56 @@
+export function renderPetCard(pet) {
+    const div = document.createElement('div');
+    const a = document.createElement('a');
+    const img = document.createElement('img');
+    const p = document.createElement('p');
+
+    div.classList.add('pet-card');
+
+    p.textContent = pet.name;
+    img.src = `./assets`;
+    a.href = `./detail/?id=${pet.id}`;
+
+    div.append(p, img);
+
+    a.append(div);
+
+    return a;
+}
+
+export function renderPetDetail(pet) {
+    const div = document.createElement('div');
+    const img = document.createElement('img');
+    const descriptionEl = document.createElement('p');
+    const nameEl = document.createElement('p');
+    const ageEl = document.createElement('p');
+    const speciesEl = document.createElement('p');
+    const breedEl = document.createElement('p');
+    const ageAndBreedEl = document.createElement('div');
+
+    div.classList.add('pet-detail');
+
+    nameEl.textContent = pet.name;
+    nameEl.classList.add('name');
+
+    descriptionEl.textContent = pet.description;
+    descriptionEl.classList.add('description');
+
+    speciesEl.textContent = pet.species;
+    speciesEl.classList.add('species');
+
+    ageEl.textContent = `${pet.age} years old`;
+    ageEl.classList.add('age');
+
+    breedEl.textContent = pet.breed;
+    breedEl.classList.add('breed');
+
+    ageAndBreedEl.classList.add('age-and-breed');
+
+    ageAndBreedEl.append(ageEl, breedEl);
+
+    img.src = `../assets/`;
+
+    div.append(nameEl, img, speciesEl, ageAndBreedEl, descriptionEl);
+
+    return div;
+}
